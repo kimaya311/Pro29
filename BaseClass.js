f1ac28733014c4926785670287438c30f9dd485a
kimaya311 <67595973+kimaya311@users.noreply.github.com>
2020-07-31 21:31:51 +0530

    Add files via upload

---
+++ BaseClass.js
@@ -0,0 +1,24 @@
+class BaseClass {
+
+constructor(x,y,width,height){
+     
+    var options = {
+        'restitution':0.8,
+        'friction':1.0,
+        'density':1.0
+         }
+    this.body = Bodies.rectangle(x,y,width,height,options);
+    this.width = width;
+    this.height = height;
+    World.add(world,this.body);
+    }
+    display(){
+        var pos = this.body.position;
+        rectMode(CENTER);
+        //fill(150,205,231);
+        rect(pos.x,pos.y,this.width,this.height);
+    }
+    
+    
+    
+    };
